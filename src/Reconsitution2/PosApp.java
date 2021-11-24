c6bebce656e3d36abd4754ff05b915ed18dfda2a
pitbull <1354863945@qq.com>
2021-11-24 21:47:58 +0800

    PosApp v1.6 状态模式重构

--- src/Reconsitution2/PosApp.java
+++ src/Reconsitution2/PosApp.java
@@ -1,48 +1,163 @@
 package Reconsitution2;
 
+
+interface State{
+    void makeNewSale(PosApp pos);
+    void enterItem(PosApp pos);
+    void finishSale(PosApp pos);
+    void makePayment(PosApp pos);
+}
+
+class NewSaleState implements State{
+
+    @Override
+    public void makeNewSale(PosApp pos) {
+       while (!pos.makeNewSale()){
+           pos.setState(this);
+       }
+    }
+
+    @Override
+    public void enterItem(PosApp pos) {
+
+    }
+
+    @Override
+    public void finishSale(PosApp pos) {
+
+    }
+
+    @Override
+    public void makePayment(PosApp pos) {
+
+    }
+}
+
+class SaleState implements State{
+
+    @Override
+    public void makeNewSale(PosApp pos) {
+
+    }
+
+    @Override
+    public void enterItem(PosApp pos) {
+        while (!pos.enterItem()){
+            pos.setState(this);
+        }
+    }
+
+    @Override
+    public void finishSale(PosApp pos) {
+
+    }
+
+    @Override
+    public void makePayment(PosApp pos) {
+
+    }
+}
+
+class PaymentState implements State{
+
+    @Override
+    public void makeNewSale(PosApp pos) {
+
+    }
+
+    @Override
+    public void enterItem(PosApp pos) {
+
+    }
+
+    @Override
+    public void finishSale(PosApp pos) {
+
+    }
+
+    @Override
+    public void makePayment(PosApp pos) {
+        while (!pos.makePayment()){
+            pos.setState(this);
+        }
+    }
+}
+
+class FinishState implements State{
+
+    @Override
+    public void makeNewSale(PosApp pos) {
+
+    }
+
+    @Override
+    public void enterItem(PosApp pos) {
+
+    }
+
+    @Override
+    public void finishSale(PosApp pos) {
+        while (!pos.finishSale()){
+            pos.setState(this);
+        }
+    }
+
+    @Override
+    public void makePayment(PosApp pos) {
+
+    }
+}
+
 public class PosApp {
     private Payment payment;
     private Sale sale;
     private boolean isdone = false;
 
-    private enum STATE {
-        WELCOMESTATE,
-        SALESTATE,
-        PAYMENTSTATE,
-        RECEPTSTATE,
+    private State state;
+
+
+
+    public State getState() {
+        return state;
     }
 
-    private STATE state = STATE.WELCOMESTATE;
+    public void setState(State state) {
+        this.state = state;
+    }
 
-    public void makeNewSale() {
+    public boolean makeNewSale() {
+        boolean res;
         try {
             Thread.sleep(0);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         WelcomeUI welcomeUI = WelcomeUI.getInstance();
+        res = welcomeUI.getIsdone();
         if (welcomeUI.getIsdone()) {
-            state = STATE.SALESTATE;
-            WelcomeUI.setNull();
+            welcomeUI.setNull();
         }
+        return res;
     }
 
-    public void enterItem() {
+    public boolean enterItem() {
+        boolean res;
         try {
             Thread.sleep(0);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
         SaleUI saleUI = SaleUI.getInstance();
         sale = SaleUI.getInstance().getSale();
+        res = saleUI.getIsdone();
         if (saleUI.getIsdone()) {
-            state = STATE.PAYMENTSTATE;
             SaleUI.setNull();
         }
+        return res;
     }
 
-    public void makePayment() {
+    public boolean makePayment() {
+        boolean res;
         try {
             Thread.sleep(0);
         } catch (InterruptedException e) {
@@ -50,44 +165,38 @@ public class PosApp {
         }
         PaymentUI paymentUI = PaymentUI.getInstance(sale);
         payment = paymentUI.getPayment();
+        res = paymentUI.getIsdone();
         if (paymentUI.getIsdone()) {
-            state = STATE.RECEPTSTATE;
             PaymentUI.setNull();
         }
+        return res;
     }
 
-    public void print() {
+    public boolean finishSale() {
+        boolean res;
         try {
             Thread.sleep(0);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         ReceptUI receptUI = ReceptUI.getInstance(sale, payment);
-
+        res = receptUI.getIsdone();
         if (receptUI.getIsdone()) {
-            state = STATE.WELCOMESTATE;
             ReceptUI.setNull();
         }
+        return res;
     }
 
     public void run() {
         while (true) {
-            switch (state) {
-                case WELCOMESTATE:
-                    makeNewSale();
-                    break;
-                case SALESTATE:
-                    enterItem();
-                    break;
-                case PAYMENTSTATE:
-                    makePayment();
-                    break;
-                case RECEPTSTATE:
-                    print();
-                    break;
-                default:
-                    break;
-            }
+            NewSaleState newSaleState = new NewSaleState();
+            newSaleState.makeNewSale(this);
+            SaleState saleState = new SaleState();
+            saleState.enterItem(this);
+            PaymentState paymentState = new PaymentState();
+            paymentState.makePayment(this);
+            FinishState finishState = new FinishState();
+            finishState.finishSale(this);
         }
     }
 
